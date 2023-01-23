57fc82497b6ce7900e9f3a22f6f779cbe7952cfb
wchristian84 <99511387+wchristian84@users.noreply.github.com>
2023-01-23 12:25:02 -0600

    Update albums_by_artist.rb

--- lib/albums_by_artist.rb
+++ lib/albums_by_artist.rb
@@ -3,6 +3,7 @@
 require_relative "albums_by_artist/version"
 require_relative "cli.rb"
 require_relative "scraper.rb"
+require_relative "user.rb"
 
 module AlbumsByArtist
   class Error < StandardError; end
